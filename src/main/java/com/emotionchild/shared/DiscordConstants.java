472e4b5530a24d9d6112214755caf38cbc3b49f0
Emotion <emotion@elliebot.net>
2023-04-15 00:26:40 +1200

    Stuff

---
+++ src/main/java/com/emotionchild/shared/DiscordConstants.java
@@ -0,0 +1,14 @@
+package com.emotionchild.shared;
+
+public class DiscordConstants {
+
+    public static final String DEFAULT_COMMAND_PREFIX = "!";
+    public static final String DEFAULT_SYSTEM_PREFIX = ";";
+
+    public static final long LIAREL_BOT_ID = 837876849416732735L;
+    public static final long KIANA_BOT_ID = 828406931336200202L;
+    public static final long FEEDBACK_CHANNEL_ID = 1096408011829956678L;
+    public static final long CHANGELOG_CHANNEL_ID = 1096408106898034889L;
+    public static final long ACTIVITY_LOG_CHANNEL_ID = 1086224963368931389L;
+    public static final long BOT_ADMIN_EXCEPTION_ROLE = 234542843732033537L;
+}
